8044de10ea778bc9b33cc1dbeafb02081e1c66aa
Wei Ning <macgnw@gmail.com>
2018-03-15 00:27:02 +0000

    - io roundtrip test; cmake

--- csockets/tests/chapter1/createSocket.c
+++ csockets/tests/chapter1/createSocket.c
@@ -11,23 +11,37 @@ void RunTinyTests();
 // netstat -p 
 // expect to see the name of this executable to show in the program column
 void test_createSocketExpectSuccess() {
+    int z;
     int s[2];
-    assert(0 == socketpair(AF_LOCAL, SOCK_STREAM, 0, s));
+    z = socketpair(AF_LOCAL, SOCK_STREAM, 0, s);
+    assert(0 == z);
 }
 
 void test_createSocketExpectFailure() {
+    int z;
     int s[2];
-    assert(-1, socketpair(-1, -1, 0, s));
+    z = socketpair(-1, -1, 0, s);
+    assert(-1 == z);
 }
 
 void test_performIOExpectSuccess() {
+    int z;
     int s[2];
     char *cp;
     char buf[80];
-    assert(0 == socketpair(AF_LOCAL, SOCK_STREAM, 0, s));
-    
-
-
+    z = socketpair(AF_LOCAL, SOCK_STREAM, 0, s);
+    assert(0 == z);
+    z = write(s[1], "dOOm!", 5);
+    assert(5 == z);
+    z = read(s[0], buf, sizeof(buf));
+    assert(5 == z);
+    buf[z] = '\0';
+    z = strcmp("dOOm!", buf);
+    assert(0 == z);
+    z = close(s[0]);
+    assert(0 == z);
+    z = close(s[1]);
+    assert(0 == z);
 }
 
 int main(int argc, char **argv) {
