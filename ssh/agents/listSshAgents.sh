53a81cfae7a1548cf4fe03c37b8fffc7535b1c27
Wei Ning <nwcgnw@gmail.com>
2018-03-28 07:05:47 +1300

    - init new example: list ssh agents

--- ssh/agents/listSshAgents.sh
+++ ssh/agents/listSshAgents.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
-function listSshAgents() {
+# good reference:
+# https://github.com/wwalker/ssh-find-agent/blob/master/ssh-find-agent.sh
+
+function listSshAgentPids() {
     ps -ALL | grep ssh-agent | awk -c 'NF > 2 && /^[ 0-9]+/ { print $1 }'
 }
 
+function getParentPid() {
+    ps -o ppid= -p ${1:-"require pid"}
+}
+
 function run() {
-    listSshAgents
+    local parentPid=""
+    for saPid in $( listSshAgentPids )
+    do
+        parentPid=$( getParentPid ${saPid} )
+        printf "%s, parent is %s" ${saPid} ${parentPid}
+    done
 }
 
 run
