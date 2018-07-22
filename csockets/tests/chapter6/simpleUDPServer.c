d5470b8afbc84756802242618995986611a6a2de
Wei Ning <nwcgnw@gmail.com>
2018-07-22 20:29:10 +1200

    - update chapter 6 udp server example

--- csockets/tests/chapter6/simpleUDPServer.c
+++ csockets/tests/chapter6/simpleUDPServer.c
@@ -9,6 +9,7 @@
 #include <assert.h>
 #include <memory.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int createAndBindSocket(const char* i_addrStr, unsigned short i_port) {
     struct sockaddr_in addr;
@@ -32,6 +33,16 @@ int createAndBindSocket(const char* i_addrStr, unsigned short i_port) {
 // receive the sender's address at the same time you receive your
 // datagram.
 
+// P172
+// (notes on recvfrom())
+// this function call will block until the server receives a datagram
+// upon return from this function, o_buffer will contain the datagram ,
+// and return value will be the size of this  datagram (or -1 if there
+// was an error returned)
+// the sender's socket address is returned in structure o_sender and
+// variable io_senderLen is set to the length of the address in
+// o_sender
+
 int receive(
     int i_socket,
     size_t i_bufferLen,
@@ -48,10 +59,32 @@ int receive(
         io_senderLen
     );
     assert(bytesReceived >= 0);
+
+    // By Example P172
+    // the recommended way is to set a 0 character
+    // o_buffer[result] = 0;
+    return 1;
 }
 
-int main() {
-    int s = createAndBindSocket("127.0.0.1", 9001);
+int main(int argc, char** argv) {
+
+    // By Example 173
+    // if you run the server with a different IP address, if you don't
+    // have an interface with this number you will get an error
+    // (can not assign request address)
+    //
+    // however when using the 127.0.0.1 network (local loopback device
+    // you can specify any class A address, if it starts with 127
+    // (the example in the book uses 127.0.0.23; my test uses
+    // 127.0.0.123)
+
+    int s = -1;
+    if (argc == 3) {
+        s = createAndBindSocket(argv[1], (unsigned short)atoi(argv[2]));
+    } else {
+        s = createAndBindSocket("127.0.0.1", 9001);
+    }
+
     struct sockaddr_in sender;  // to be populated by recvfrom()
     socklen_t senderLen = sizeof(sender);  // updated by recvfrom()
 
@@ -63,7 +96,10 @@ int main() {
     receive(s, messageLen, message, &sender, &senderLen);
 #endif
 
-    printf("%s\n", message);
+    printf("client (%s:%d) message: %s\n",
+           inet_ntoa(sender.sin_addr),
+           sender.sin_port,
+           message);
 
     close(s);
     return 0;
--- csockets/tests/chapter6/simpleUDPServer.sh
+++ csockets/tests/chapter6/simpleUDPServer.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
+# note
+# observe the sender's address and port each time this demo runs:
+# client (127.0.0.1:31366) message: iddqd
+# the address is localhost, but the port is randomly chosen (by netcat)
+
 CC=gcc
 SRC=./simpleUDPServer.c
 BIN=/tmp/sut/out
+SRV_ADDR=127.0.0.123
+SRV_PORT=9001
 
 setUp() {
     set -e
@@ -15,10 +22,10 @@ buildProgram() {
 }
 
 runProgram() {
-    ${BIN} &
+    ${BIN} ${SRV_ADDR} ${SRV_PORT} &
     sleep 0.1
 
-    echo "iddqd" | netcat -w 0 -u 127.0.0.1 9001
+    echo "iddqd" | netcat -w 0 -u ${SRV_ADDR} ${SRV_PORT}
     wait
 }
 
