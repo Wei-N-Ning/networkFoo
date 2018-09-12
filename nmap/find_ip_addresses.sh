25d1b17bd10b1201609eede0e893aab9b6d8cef0
Wei Ning <nwcgnw@gmail.com>
2018-09-13 11:53:07 +1200

    - add address scanning example

---
+++ nmap/find_ip_addresses.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash 
+
+# source
+# https://stackoverflow.com/questions/13669585/how-to-get-a-list-of-all-valid-ip-addresses-in-a-local-network
+
+# sudo apt-get install nmap
+
+# then
+
+nmap -sP 192.168.1.*
