a1c33d628a7e7fcbefd52f31895ad0e20ecb52a9
Wei.n <macgnw@gmail.com>
2018-12-23 21:06:41 +1100

    ssh on mac

---
+++ ssh/mac_os_enable_ssh.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+enable_ssh() {
+    # system preference -> sharing -> remote 
+
+    # add the superuser/myself
+
+    # test it on another workstation in the same subnet
+    :
+}
