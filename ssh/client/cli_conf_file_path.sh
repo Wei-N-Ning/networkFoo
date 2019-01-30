31867d1e52861599a3b9e8b2ae3a843b2b16880b
Wei Ning <wei@canva.com>
2019-01-30 12:02:16 +1100

    update ssh -F

--- ssh/client/cli_conf_file_path.sh
+++ ssh/client/cli_conf_file_path.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 # use explicit configure file path
+# this is used in ca code base to allow "flavored" client-side conf,
+# such as ~/.ssh/kknd.config, ~/.ssh/doom.config
+# the ssh command uses -F to ensure the same flavor is 
+# used in both the configure file name and other parts 
 conf_file_path() {
     cp ${HOME}/.ssh/config /var/tmp/cli_conf.test
     ssh -F /var/tmp/cli_conf.test "user@address" -p port
