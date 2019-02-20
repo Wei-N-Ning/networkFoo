67a8ef95c7577377fa3a05c91145241c78867203
Wei Ning <macgnw@gmail.com>
2019-02-20 20:41:04 +1100

    ssh

--- ssh/mac_os_enable_ssh.sh
+++ ssh/mac_os_enable_ssh.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# I can also use sshd debugging mode to temporarily open a 2222 
+# etc port for an one-off connection.
+
 enable_ssh() {
     # system preference -> sharing -> remote 
 
