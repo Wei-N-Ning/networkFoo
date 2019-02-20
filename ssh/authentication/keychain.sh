ccb3f1dd38c2c792ab6c0d2a628941f4a7f8456f
Wei Ning <wei@canva.com>
2019-02-20 14:16:34 +1100

    ssh review

--- ssh/authentication/keychain.sh
+++ ssh/authentication/keychain.sh
@@ -5,9 +5,9 @@
 # not to be mixed up with ~/.aws/cli/cache directory!
 
 # there is an extre twist in jump server's /etc/ssh/sshd_config
-# see authorized_keys_command.sh
-# it looks up the key file from the current user name therefore the log in user 
-# does not need to provide pem/-i !
+# see CA's authorized_keys_command.sh
+# it looks up the key file use user "iam" (which is sort of a god-user) 
+# therefore the login user does not need to provide pem/-i !
 
 # good articles and Funtoo linux (Gentoo linux variation)
 
--- ssh/authentication/print_rsa_fingerprint.sh
+++ ssh/authentication/print_rsa_fingerprint.sh
@@ -15,10 +15,35 @@
     #  clients to the ssh-agent program.  Through use of environment variables the agent can be located and automatically used
     #  for authentication when logging in to other machines using ssh(1).
 
+
+# motivation:
+# there seems no good way to "view" the finger print of a RSA key pair other than
+# using ssh agent that:
+# 1. firstly adds the key pair (done via ssh-add)
+# 2. secondly list the key pair (done via ssh-add -l)
+# but calling ssh-add will affect the current calling environment,
+# the follow recipe is to call ssh-agent in a subprocess hence avoiding 
+# the pollution.
 print_rsa_fingerprint() {
     # this sh process quickly terminates therefore the current
     # calling environment is unaffected
+    # 
+    # man page says:
+    # If a command line is given, this is executed as a subprocess of the agent. 
+    # When the command dies, so does the agent.
+    # 
+    # The idea is that the agent is run in the user's local PC, laptop, or terminal.  
+    # Authentication data need not be stored on any other machine, and authentication 
+    # passphrases never go over the network.  However, the connection to the agent is 
+    # forwarded over SSH remote logins, and the user can thus use the privileges given 
+    # by the identities anywhere in the network in a secure way.
+    # 
+    # clear all the entries in the calling environment
+    ssh-add -D 
+    # "view" the finger print
     ssh-agent sh -c 'ssh-add; ssh-add -l'
+    # verify that the calling environment is unaffected 
+    ssh-add -l
 
     # I can abuse this to also print the fingerprint of 
     # my encrypted key
