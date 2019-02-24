7fcdecec263abae9cd444258f0aa6ca8a245bca8
Wei Ning <wei@canva.com>
2019-02-25 09:07:36 +1100

    layers

---
+++ connection/firewall.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# source
+# networking for systems admin, L1428
+# common firewalls use a default deny policy.
+# everything is forbidden unless explicitly permitted
+# firewalls are not the only devices that can control access
+# to specific address, protocols and ports, 
+# most routers and many ethernet switches can perform packet 
+# filtering or use access control list (ACLS) to limit
+# connectivity
+
+# NOTE: recall AWS' NACL - understand where this concept comes 
+# from.
+
+
+
+
+
---
+++ dns/concepts.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# source
+# networking for systems admin, L1452
+
+# DNS runs on TCP and UDP, using port 53 on both
+# every network needs a DNS server, also called a nameserver, 
+# to gather this information for you.
+
+# a nameserver is a piece of software that searches for and 
+# collects address and hostname mappings. Whenever you visit 
+# a web page your computer makes a DNS request to a 
+# nameserver. The nameserver checks its local cache to see if 
+# it already has an answer. If the nameserver has a cached
+# answer it sends the information to the client,
+# if the nameserver does not have that info it queries the 
+# Internet to get an answer
+
+
+
+
+
+
+
