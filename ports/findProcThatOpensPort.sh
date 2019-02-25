5f082ee97a56509f61e7b081ca9c0001ee22fe4c
Wei Ning <wei@canva.com>
2019-02-25 19:52:19 +1100

    notes

--- ports/findProcThatOpensPort.sh
+++ ports/findProcThatOpensPort.sh
@@ -3,5 +3,15 @@
 # source
 # https://www.cyberciti.biz/faq/what-process-has-open-linux-port/
 
-fuser 7000/tcp 2>/dev/null
+fuser_demo() {
+    # fuser: identify processes using files or sockets
 
+    fuser 7000/tcp 2>/dev/null
+
+    # tested this in CA dev.jump host (after ssh-ing to it)
+    fuser 22/tcp -v
+    #                      USER        PID ACCESS COMMAND
+    # 22/tcp:              root       3694 F.... sshd
+    #                      ubuntu     3782 F.... sshd
+    #                      root      20225 F.... sshd
+}
