4a6ba2d7a93b8b8d2a98891cf64166066cf6a379
Wei Ning <wei@canva.com>
2019-02-25 22:11:08 +1100

    dns

--- dns/concepts.sh
+++ dns/concepts.sh
@@ -16,7 +16,19 @@
 # if the nameserver does not have that info it queries the 
 # Internet to get an answer
 
+# L1513
+# DNS was designed as a general purpose configuration database,
+# mostly widely used to map IP to hostnames and back...
+# it worked, so over the years people have jammed all sorts 
+# of interesting things into zone records.
+# they got away with it so people added more data types .... 
 
+# when you find incorrect DNS information, see if it is coming
+# from the host's local cache, the recursive nameserver's 
+# cache or the authoritative server
+
+# when you suspect a DNS problem, check to see if you are 
+# getting an answer from a nameserver or a local cache.
 
 
 
