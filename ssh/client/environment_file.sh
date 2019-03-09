32a730d50ee254856fa872a8a3abdb4e7e4cbde9
Wei Ning <wei@canva.com>
2019-03-09 15:23:18 +1100

    ssh

---
+++ ssh/client/environment_file.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# NOT WORKING FOR ME
+
+# SSH definitive 2nd P/296
+# see also man ssh
+
+# environment variables may be specified in a file,
+# /etc/environment
+# ~/.ssh/environment (open ssh only)
+
+# Name=Value
+# ////////
+# NOTE PermitUserEnvironment yes must be set in sshd_config
+# ////////
+
--- ssh/transfer/scp_with_port.sh
+++ ssh/transfer/scp_with_port.sh
@@ -5,6 +5,8 @@
 
 # use scp's special protocol syntax
 copy_files_via_port_2222() {
+    # use -P for port
+    # scp 'options...' -P 2222 'local_filename' user@host:'remote_location' 
     scp -r aws-sec scp://weining@10.0.1.67:2222//var/tmp
 }
 
