032706dc2a290169cd4061d44d61452b47bd841c
Wei Ning <wei@canva.com>
2019-03-10 22:13:58 +1100

    ssh

---
+++ firewall/iptables/to_read.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# https://www.udemy.com/linux-security/learn/v4/t/lecture/4626790?start=0
+
---
+++ tcp_wrapper/to_read.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# https://www.udemy.com/linux-security/learn/v4/t/lecture/4613660?start=0
+
+
