f0d11ca2319d9edb73e01006ca0d728a98d8c9ab
Wei Ning <wei@canva.com>
2019-05-17 09:08:35 +1000

    wip

---
+++ http/http2.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/env perl
+
+=pod
+source:
+udemy gRPC [golang] master class
+
+https://imagekit.io/demo/http2-vs-http1
+
+gRPC only uses http 2, fast (multiplex), secure (SSL enabled)
+=cut
