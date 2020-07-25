415ce3ad7ffd930896d7157bd5b6a964fd2eb105
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-07-25 15:28:39 +1000

    wip

--- communicate/curl_put_json.sh
+++ communicate/curl_put_json.sh
@@ -2,6 +2,7 @@
 
 # --header '<set key-values>'
 # -d '<stringified json>'
+# if -d is given and -X is omitted, the action by default is POST
 curl -X PUT --header 'content-type: application/json' -v 'http://0.0.0.0:8000/README.md' -d '{"a": "b"}'
 
 # data is stored in the body section
