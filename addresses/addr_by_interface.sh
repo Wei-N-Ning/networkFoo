a9bbca0a03a432596f474925c820cc04144d7db5
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-08-04 17:56:45 +1000

    wip

--- addresses/addr_by_interface.sh
+++ addresses/addr_by_interface.sh
@@ -2,7 +2,7 @@
 
 # source:
 # https://unix.stackexchange.com/questions/54975/how-to-check-that-a-daemon-is-listening-on-what-interface
-function run() {
+run() {
     local address=${1:-127.0.0.1}
     for i in $(grep ':' /proc/net/dev | cut -d ':' -f 1 | tr -d ' ') ; do
             if $(ip address show dev ${i} | grep -q "${address}") ; then
@@ -11,4 +11,4 @@ function run() {
     done
 }
 
-run
+run $@
