1b65c57d4c975d0a6091318c2bc27c3b9a1c1578
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-08-17 17:58:43 +1000

    wip

--- routes/traceroute.sh
+++ routes/traceroute.sh
@@ -11,3 +11,15 @@
 # the path taken can be also studied as part of static perf
 # tuning.
 
+# like ping, can omit the 0s inbetween
+traceroute 1.1
+
+# Unix/Linux traceroute uses udp (not icmp); windows uses icmp; 
+# observe this:
+sudo tcpdump -v -i wlp59s0 src 192.168.1.106 and dst 1.0.0.1 and udp
+
+# there is clearly some caching going on!
+# ttl is incremented per route
+
+# let traceroute use icmp packets instead
+sudo traceroute -I 1.1
