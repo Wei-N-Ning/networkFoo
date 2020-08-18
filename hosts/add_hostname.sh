991701a560ad259e67ee5bb64cb95a172b69ac00
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-08-18 11:51:15 +1000

    wip

---
+++ hosts/add_hostname.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# motivation:
+# I have launched a couple of new Vmware vms, how can I ssh to
+# them using their hostname (instead of ip addr)?
+
+# source: https://unix.stackexchange.com/questions/55691/how-to-add-an-ip-to-hostname-file
+# sudo vim /etc/hosts
+# <ip addr> <hostname>
