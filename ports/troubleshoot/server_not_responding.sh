fac6216a9c3aae84749d2cbae0bfdb2a46da56a8
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-08-23 19:58:24 +1000

    wip

--- ports/troubleshoot/server_not_responding.sh
+++ ports/troubleshoot/server_not_responding.sh
@@ -3,6 +3,7 @@
 # server is not responding
 
 # is server listening to its port(s)?
+# also run systemctl status <service name> to check its status
 check_ports_listening() {
     netstats -lnp "$(pgrep server_executable)"
 }
