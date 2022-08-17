31565ed60f6cf654165333fa1773462664128a2f
Wei-N-Ning <macgnw@gmail.com>
2022-08-17 19:48:38 +1000

    notes

--- subnet/address_in_range.py
+++ subnet/address_in_range.py
@@ -21,3 +21,21 @@ rang = ipaddress.ip_network('10.0.0.0/28')
 assert addr in rang
 
 assert ipaddress.ip_address('10.0.0.212') not in rang
+
+# note, I can do it the hard way:
+def address_bits(xs):
+    """given an IPv4 address as a list of four integers, return its bit-representation"""
+    return ' '.join(['{:08b}'.format(x) for x in xs])
+
+print(address_bits([10, 0, 0, 1]))
+print(address_bits([10, 0, 0, 212]))
+print(address_bits([0b11111111, 0b11111111, 0b11111111, 0b11110000]))
+
+'''
+00001010 00000000 00000000 00000001
+00001010 00000000 00000000 11010100
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^these bits must be identical or these two
+                               addresses are in the same subnet range                              
+11111111 11111111 11111111 11110000
+
+'''
